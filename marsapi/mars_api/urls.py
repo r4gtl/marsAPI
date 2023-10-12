be5ffe612ebd0f42cb5d8b45d0c7dc0793de020b
r4gtl <rquattrogtl@gmail.com>
2023-10-12 17:35:36 +0200

    Setting API requests

--- marsapi/mars_api/urls.py
+++ marsapi/mars_api/urls.py
@@ -4,5 +4,6 @@ from .views import home
 app_name="mars_api"
 
 urlpatterns = [
-    path('', home, name='home'),
+    path('', home, name='home'),  # Questa riga gestisce la radice del sito web
+    path('<str:rover_name>/', home, name='home'),
 ]
--- marsapi/mars_api/views.py
+++ marsapi/mars_api/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.conf import settings
 from datetime import datetime
+from django.core.cache import cache
 
 import requests
 import json
@@ -8,67 +9,65 @@ import json
 # Create your views here.
 
 
-def home(request):
-    landing_date = datetime(2012, 8, 6) 
-    current_date = datetime.now()
-    delta = current_date - landing_date
-    current_sol = delta.days
+def home(request, rover_name='curiosity'):
+
     
-    response = requests.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=3974&api_key=' + settings.NASA_API_KEY)
+    background_response = requests.get(f'https://api.nasa.gov/planetary/apod?api_key={settings.NASA_API_KEY}')
+    response = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/{rover_name}/photos?sol=100&page=1&api_key={settings.NASA_API_KEY}')
     
+    headers = response.headers
+    print(f'headers: {headers}')
     
-    print(f"NASA_API_KEY: {settings.NASA_API_KEY}")
-    print(f"Response: {response}")
-    
+    loaded_background_json = json.loads(background_response.text)
     loaded_json = json.loads(response.text)
-    print(f"loaded_json: {loaded_json}")
+    #print(f"loaded_background_json: {loaded_background_json}")
+    #print(f"loaded_background_json_url: {loaded_background_json['url']}")
+    #print(f"loaded_json: {loaded_json}")
+    background_src=loaded_background_json['url']
+    rover_name = loaded_json['photos'][0]['rover']['name']
+    launch_date = loaded_json['photos'][0]['rover']['launch_date']
+    date_obj = datetime.strptime(launch_date, "%Y-%m-%d")
+    launch_date = date_obj.strftime("%d/%m/%Y")
+    landing_date = loaded_json['photos'][0]['rover']['landing_date']
+    date_obj = datetime.strptime(landing_date, "%Y-%m-%d")
+    landing_date = date_obj.strftime("%d/%m/%Y")
     max_sol = loaded_json['photos'][0]['rover']['max_sol']
     images_list = []
-    for i in range(max_sol, max_sol - 7, -1):
-        response = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol={i}&api_key={settings.NASA_API_KEY}')        
+    for i in range(max_sol, max_sol - 6, -1):
+        response = requests.get(f'https://api.nasa.gov/mars-photos/api/v1/rovers/{rover_name}/photos?sol={i}&page=1&api_key={settings.NASA_API_KEY}')   
+        headers = response.headers     
+        print(f'headers: {headers}')
         if response.status_code == 200:
             loaded_json = json.loads(response.text)
+            print(f"loaded_jsonaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa: {loaded_json}")
+            
             if 'photos' in loaded_json and len(loaded_json['photos']) > 0:
-                img_src = loaded_json['photos'][0]['img_src']
-                earth_date = loaded_json['photos'][0]['earth_date']
+                prima_immagine = loaded_json['photos'][0]
+                img_src = prima_immagine['img_src']
+                earth_date = prima_immagine['earth_date']
                 date_obj = datetime.strptime(earth_date, "%Y-%m-%d")
                 earth_date = date_obj.strftime("%d/%m/%Y")
-                sol = loaded_json['photos'][0]['sol']
+                sol = prima_immagine['sol']
+                #img_src = loaded_json['photos'][0]['img_src']
+                #earth_date = loaded_json['photos'][0]['earth_date']
+                #date_obj = datetime.strptime(earth_date, "%Y-%m-%d")
+                #earth_date = date_obj.strftime("%d/%m/%Y")
+                #sol = loaded_json['photos'][0]['sol']
+                
                 images_list.append({'img_src': img_src, 'earth_date': earth_date, 'sol': sol})
             
-    print(f"images_list: {images_list}")
-
-    #try:
-    #    img_src = loaded_json['photos'][0]['img_src']
-    #    earth_date = loaded_json['photos'][0]['earth_date']
-    #    date_obj = datetime.strptime(earth_date, "%Y-%m-%d")
-    #    earth_date = date_obj.strftime("%d/%m/%Y")
-    #    sol = loaded_json['photos'][0]['sol']#
-
-        #daily_image = loaded_json.get('url')
-        #print(f"daily_image: {daily_image}")
-    #except IndexError:
-        # Gestisci l'errore qui
-    #    img_src = ""
-    #    earth_date = "Il giorno richiesto non esiste"
-    #    sol = "Il sol richiesto non esiste"
-    
     
 
-    #title = loaded_json.get('title')
-    #explanation = loaded_json.get('explanation')
-    #date = loaded_json.get('date')
-    #owner = loaded_json.get('copyright')
+    
 
     context = {
-        #'img_src': img_src,
-        #'earth_date': earth_date,
-        #'sol': sol,
         'images_list': images_list,
-        #'title':title,
-        #'explanation':explanation,
-        #'date':date,
-        #'owner':owner
+        'background_src':background_src,
+        'rover_name': rover_name,
+        'launch_date': launch_date,
+        'landing_date': landing_date
+
+
     }
 
     return render(request, "mars_api/home.html", context)
